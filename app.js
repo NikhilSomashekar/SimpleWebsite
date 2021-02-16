725508bce430f1cf38c7deef1bd32d50b950583c
Nikhil Somashekarappa <nikhildvg96@gmail.com>
2021-02-16 15:25:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+        document.body.classList.toggle('light-theme')
+    }
+    console.log('current class name: ' + className);
+});
